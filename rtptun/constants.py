26448239a38ea24c8c0cd51fe26d323acba96c35
Mehrzad Asri <mehrzad.asri@protonmail.ch>
2022-10-19 10:52:49 +0330

    Correct payload type dynamic range

--- rtptun/constants.py
+++ rtptun/constants.py
@@ -7,7 +7,7 @@ class Constants:
     TIMEOUT: int = 120
     TIMESTAMP_INCREMENT = 3000  # 90kHz / 30FPS video
     # While the actual RTP payload dynamic range is 96 to 100 the safe range (at least here) seems to be 96 to 98
-    DYNAMIC_RANGE = (96, 100)
+    DYNAMIC_RANGE = (96, 98)
 
     UINT16_MAX: int = (2 ** 16) - 1
     UINT32_MAX: int = (2 ** 32) - 1
